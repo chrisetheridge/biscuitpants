b278eb26ceb9f6dcc325158b420b86475b1fe6e6
biscuitpants <chrise@cognician.com>
2020-03-26 11:43:50 +0200

    Initial

---
+++ config.el
@@ -0,0 +1,130 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Chris Etheridge"
+      user-mail-address "chrise@cognician.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Hack" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-opera)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(setq company-idle-delay 0.1)
+(setq company-show-numbers t)
+(setq company-tooltip-align-annotations t)
+
+;; windmove
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings 'super))
+
+;; Projectile
+(after! projectile
+  ;; ignored files
+  (setq projectile-globally-ignored-files
+        (cl-union projectile-globally-ignored-files
+                  '(".DS_Store"
+                    ".lein-repl-history"
+                    "*.gz"
+                    "*.pyc"
+                    "*.png"
+                    "*.jpg"
+                    "*.jar"
+                    "*.retry"
+                    "*.svg"
+                    "*.tar.gz"
+                    "*.tgz"
+                    "*.zip")))
+  ;; ignored directories
+  (setq projectile-globally-ignored-directories
+        (cl-union projectile-globally-ignored-directories
+                  '(".git"
+                    ".ignore"
+                    ".cljs_rhino_repl"
+                    ".svn"
+                    "out"
+                    "node_modules"
+                    "resources/public/js/compiled"
+                    ".shadow-cljs/"))))
+
+;; Clojure
+(after! clojure-mode
+  (setq clojure-align-forms-automatically t)
+  (define-clojure-indent
+    ;; Compojure
+    (GET 'defun)
+    (POST 'defun)
+    (context 'defun)
+    (cj/GET 'defun)
+    (cj/POST 'defun )
+    (cj/context 'defun)
+    ;; Tufte (profiling)
+    (tufte/p 'defun)
+    (tufte/profile 'defun)))
+
+;; CIDER (clojure)
+(after! cider
+  (setq
+   nrepl-log-messges t
+   cider-use-fringe-indicators nil
+   cider-prompt-for-symbol nil
+   cider-repl-pop-to-buffer-on-connect nil
+   cider-repl-display-in-current-window t
+   cider-show-error-buffer t
+   cider-auto-select-error-buffer nil
+   cider-save-file-on-load t
+   cider-repl-history-file (concat user-emacs-directory "cider-history")
+   cider-repl-use-clojure-font-lock t
+   cider-font-lock-dynamically t
+   cider-font-lock-reader-conditionals nil
+   cider-repl-wrap-history t
+   cider-repl-history-size 3000))
+
+(global-set-key (kbd "C-k") 'paredit-kill)
+(global-set-key (kbd "M-(") 'paredit-wrap-sexp)
+(global-set-key (kbd "M-}") 'paredit-wrap-curly)
+(global-set-key (kbd "M-[" ) 'paredit-wrap-square)
+(global-set-key (kbd "M-r") 'sp-splice-sexp-killing-around)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+
+(global-set-key (kbd "s-}") 'centaur-tabs-forward)
+(global-set-key (kbd "s-{") 'centaur-tabs-backward)
