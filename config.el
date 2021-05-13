39f8b52862db5cbfc047cbdd532e1cd91c1e0ed2
chris etheridge <chrise@cognician.com>
2021-05-13 16:02:32 +0200

    theme tweaks

--- config.el
+++ config.el
@@ -18,12 +18,12 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Hack" :size 12))
+(setq doom-font (font-spec :family "Fira Code" :size 12))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-opera)
+(setq doom-theme 'doom-flatwhite)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org/")
